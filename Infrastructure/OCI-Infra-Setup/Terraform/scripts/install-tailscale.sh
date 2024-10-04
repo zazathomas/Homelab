50965cb2794fd86fbecc851a38e5f8b6548b756e
Zaza <thomas.kyle6@gmail.com>
2024-10-04 11:21:59 +0100

    bundle fix

--- Infrastructure/OCI-Infra-Setup/Terraform/scripts/install-tailscale.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Install tailscale
-
-curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/jammy.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
-curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/jammy.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
-sudo apt-get update -y
-sudo apt-get install tailscale -y
-sudo systemctl enable --now tailscaled
-sudo tailscale up --authkey example-auth-key
--- K8s/Manifests/kube-prometheus-stack/README.md
+++
@@ -1,8 +0,0 @@
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-helm repo update
-kubectl create namespace monitoring
-create admin-username and password for grafana
-edit the values.yaml file
-helm upgrade --install -n monitoring prometheus prometheus-community/kube-prometheus-stack -f values.yaml
-apply ingress to expose service
-add ssl cert to ns
